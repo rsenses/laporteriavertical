56ece38b33d3b93127b50e67fbd03c6451a24c75
Rubén Silva <ruben.silva@expomark.es>
2021-06-15 15:08:48 +0200

    Añade home del proyecto

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -8,20 +8,13 @@ class HomeController extends BaseController
 {
     public function indexAction()
     {
-        $last = Post::fetchLast($this->db);
-
-        return $this->app->redirect('/' . $last->url);
-    }
-
-    public function indexOKAction()
-    {
-        $featured = Post::fetchFeatured($this->db);
+        $posts = Post::fetchFeatured($this->db);
 
         return $this->app->view()->render(
             'home.phtml',
             [
                 'section' => 'home',
-                'featured' => $featured
+                'posts' => $posts
             ]
         );
     }
